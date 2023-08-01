a19cfb0b6c48719bff7c5b98fb44dc33f351853e
Flynn (Mr. Robot) <flynnwebdev@gmail.com>
2023-08-01 12:04:28 +1000

    Refactored NewEntry

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,18 @@ import React, { useState } from "react"
 import Home from "./Home"
 import CategorySelection from "./CategorySelection"
 import NewEntry from "./NewEntry"
-import { BrowserRouter, Routes, Route, useParams } from "react-router-dom"
+import { BrowserRouter, Routes, Route, useParams, useNavigate } from "react-router-dom"
 import NavBar from "./NavBar"
 import ShowEntry from "./ShowEntry"
 
 const seedEntries = [
-  { category: 'Food', content: 'Pizza is yummy!' },
-  { category: 'Coding', content: 'Coding is fun!' },
-  { category: 'Gaming', content: 'Skyrim is for the Nords!' }
+  { category: "Food", content: "Pizza is yummy!" },
+  { category: "Coding", content: "Coding is fun!" },
+  { category: "Gaming", content: "Skyrim is for the Nords!" },
 ]
 
 const App = () => {
+  let nav
   const [entries, setEntries] = useState(seedEntries)
   // console.log(`id: ${id}`)
 
@@ -22,16 +23,29 @@ const App = () => {
     return <ShowEntry entry={entries[id]} />
   }
 
+  function NewEntryWrapper() {
+    nav = useNavigate()
+    return <NewEntry addEntry={addEntry} />
+  }
+
+  function addEntry(category, content) {
+    const id = entries.length
+    // Add a new entry
+    const newEntry = { category, content }
+    setEntries([...entries, newEntry])
+    nav(`/entry/${id}`)
+  }
+
   return (
     <>
       <BrowserRouter>
         <NavBar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home entries={entries} />} />
           <Route path="/category" element={<CategorySelection />} />
           <Route path="/entry">
             <Route path=":id" element={<ShowEntryWrapper />} />
-            <Route path="new/:category" element={<NewEntry entries={entries} setEntries={setEntries} />} />
+            <Route path="new/:category" element={<NewEntryWrapper />} />
           </Route>
           <Route path="*" element={<h3>Page not found</h3>} />
         </Routes>
