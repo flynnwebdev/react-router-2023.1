edfb38ddc9c97527ccb7a8f806645cc26d0b5e81
Flynn (Mr. Robot) <flynnwebdev@gmail.com>
2023-08-01 10:51:28 +1000

    Added ShowEntry

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,25 @@ import React, { useState } from "react"
 import Home from "./Home"
 import CategorySelection from "./CategorySelection"
 import NewEntry from "./NewEntry"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom"
 import NavBar from "./NavBar"
+import ShowEntry from "./ShowEntry"
+
+const seedEntries = [
+  { category: 'Food', content: 'Pizza is yummy!' },
+  { category: 'Coding', content: 'Coding is fun!' },
+  { category: 'Gaming', content: 'Skyrim is for the Nords!' }
+]
 
 const App = () => {
-  const [entries, setEntries] = useState([])
+  const [entries, setEntries] = useState(seedEntries)
+  // console.log(`id: ${id}`)
+
+  // HOC (higher-order component)
+  function ShowEntryWrapper() {
+    const { id } = useParams()
+    return <ShowEntry entry={entries[id]} />
+  }
 
   return (
     <>
@@ -16,6 +30,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/category" element={<CategorySelection />} />
           <Route path="/entry">
+            <Route path=":id" element={<ShowEntryWrapper />} />
             <Route path="new/:category" element={<NewEntry entries={entries} setEntries={setEntries} />} />
           </Route>
           <Route path="*" element={<h3>Page not found</h3>} />
