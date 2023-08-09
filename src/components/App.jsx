f084199adab0ec288681e42a0ca0917830ce36ab
Flynn (Mr. Robot) <flynnwebdev@gmail.com>
2023-08-09 10:43:47 +1000

    Added fetch entries

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import Home from "./Home"
 import CategorySelection from "./CategorySelection"
 import NewEntry from "./NewEntry"
@@ -6,15 +6,21 @@ import { BrowserRouter, Routes, Route, useParams, useNavigate } from "react-rout
 import NavBar from "./NavBar"
 import ShowEntry from "./ShowEntry"
 
-const seedEntries = [
-  { category: "Food", content: "Pizza is yummy!" },
-  { category: "Coding", content: "Coding is fun!" },
-  { category: "Gaming", content: "Skyrim is for the Nords!" },
-]
+// const seedEntries = [
+//   { category: "Food", content: "Pizza is yummy!" },
+//   { category: "Coding", content: "Coding is fun!" },
+//   { category: "Gaming", content: "Skyrim is for the Nords!" },
+// ]
 
 const App = () => {
   const nav = useNavigate()
-  const [entries, setEntries] = useState(seedEntries)
+  const [entries, setEntries] = useState([])
+
+  useEffect(async () => {
+    const res = await fetch('http://localhost:4001/entries')
+    const data = await res.json()
+    setEntries(data)
+  }, [])
 
   // HOC (higher-order component)
   function ShowEntryWrapper() {
